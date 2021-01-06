eccf075e6c6f2829b73ac4bfc2fc3cb78d1651c2
unknown <nasif.alam@mail.mcgill.ca>
2021-01-06 06:16:19 -0500

    j

--- PHP_Include_and_Require/someContent.php
+++ PHP_Include_and_Require/someContent.php
@@ -5,7 +5,7 @@
 <div class="header">
 <?php include 'someHeader.php';?>
 </div>
-<h1> Testing the incude and require functions in PHP</h1>
+<h1> Testing. the incude and require functions in PHP</h1>
 <p> Hello World! </p>
 <p> Use these ressouces </p>
 <p> https://www.w3schools.com/php/php_includes.asp </p>
