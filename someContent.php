1286e62a6609d576d15dddbd63f36f7a8a7098f3
unknown <nasif.alam@mail.mcgill.ca>
2021-01-06 05:08:22 -0500

    Modified the ReadME File to see if GIT Push is working

---
+++ someContent.php
@@ -0,0 +1,22 @@
+<?php
+
+
+
+?>
+
+<html>
+<head>
+</head>
+<body>
+<div class="header">
+<?php include 'someHeader.php';?>
+</div>
+<h1> Testing the incude and require functions in PHP </h1>
+<p> Use these ressouces </p>
+<p> https://www.w3schools.com/php/php_includes.asp </p>
+<div class="footer">
+<?php include 'someFooter.php';?>
+</div>
+</body>
+
+</html>
